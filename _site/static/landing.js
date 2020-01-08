7ddc26975f20787a4779e53b0d065ce61a090ac5
kenmingwang <fengfeng8867dd@hotmail.com>
2020-01-08 06:05:31 -0800

    fix iframe not handling mobile

--- _site/static/landing.js
+++ _site/static/landing.js
@@ -21,3 +21,13 @@ window.addEventListener("load", function() {
     getStartedSection.classList.remove("getStartedSection--gradle");
   });
 });
+
+
+function changeFrameHeight(){
+  var ifm= document.getElementById("iframepage");
+  ifm.height=document.documentElement.clientHeight;
+  }
+  window.onresize=function(){
+  changeFrameHeight();
+  }
+  
